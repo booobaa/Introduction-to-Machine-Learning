f74a136f8a5769a4e2f902106b5773a3d7e82625
Boubacar Diallo <boubaa.bd@gmail.com>
2025-11-25 18:44:25 +0000

    Structure des fichiers ajoutes

---
+++ template.sh
@@ -0,0 +1,13 @@
+# Creation des dossiers principaux 
+mkdir -p src
+mkdir -p research
+mkdir images
+# Creation des fichiers principaux
+touch src/__init__.py
+touch src/main.py
+touch research/test.py
+touch intro.ipynb
+touch .env
+touch requirements.txt
+
+echo "Les dossiers et fichiers principaux ont été créés avec succès."
